9452354b2de75b887c73eb3c0ce261695c4655cd
A <test>
2024-06-07 14:05:44 -0600

    Commit1

---
+++ src/main/java/com/mycompany/lab8p1_antoniocastillo/Lab8P1_AntonioCastillo.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.lab8p1_antoniocastillo;
+
+/**
+ *
+ * @author ac
+ */
+public class Lab8P1_AntonioCastillo {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
